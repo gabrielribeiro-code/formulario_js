8754b177bb2279f630f779eb2e3c45a1ab778d23
gabrielribeiro-code <gabriel_ribeiro16@estudante.sesisenai.org.br>
2026-03-25 09:10:10 -0300

    feat: adicionei as funções

---
+++ script.js
@@ -0,0 +1,64 @@
+let form = document.getElementById('formulario');
+
+form.addEventListener('submit', function(e){
+
+
+    e.preventDefault();
+
+
+    let valido = true;
+
+    let nome = document.getElementById('nome').value;
+    let email = document.getElementById('email').value;
+    let senha = document.getElementById('senha').value;
+
+
+    document.getElementById('erroNome').textContent ='';
+    document.getElementById('erroEmail').textContent ='';
+    document.getElementById('erroSenha').textContent ='';
+
+
+    if (nome.lenght < 3) {
+
+        document.getElementById('erroNome').textContent = 'Nome invalido, Nome deve ter pelo menos 3 caracteres.';
+
+        valido = false;
+
+    }
+
+    if(!email.includes('@')){
+
+        document.getElementById('erroEmail').textContent = 'Email Invalido.';
+        valido = false;
+    }
+
+
+    if(senha.lenght < 6) {
+
+        document.getElementById('erroSenha').textContent = 'A senha deve ter pelo menos 6 caracteres.';
+
+        valido = false;
+
+    }
+
+
+    if(valido){
+
+        let resultado = document.getElementById('resultado');
+
+        resultado.innerHTML = `
+        
+        Dados enviados: <br>
+        Nome: ${nome} <br>
+        Email: ${email} <br>
+        Senha: ${senha} <br>
+        
+        `;
+
+        form.reset();
+        
+    }
+
+
+
+})
